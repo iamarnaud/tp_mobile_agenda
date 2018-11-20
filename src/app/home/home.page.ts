2a420aab1073d02ac15bdd1f9d1c56caf0c411db
IpCeSs <ceciliapigeolet@gmail.com>
2018-11-20 18:45:44 +0100

    add days of week

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,9 @@ export class HomePage {
     localeString: string = 'fr';
     // gets todays date
     navDate: any;
+   // month: string[] = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+    days: string[] = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
 
     ngOnInit() {
         moment.locale(this.localeString);
@@ -20,17 +23,17 @@ export class HomePage {
     // numToChange indique s'il faut augmenter ou diminuer (dans html -1 / 1 param 1)
     changeNavMonth(numToChange: number) {
         // si view date est bien dans l'interval des dates que l'on a défini
-        if (this.canChangeNavMonth(numToChange)) {
+       // if (this.canChangeNavMonth(numToChange)) {
             this.navDate.add(numToChange, 'month');
-        }
+       // }
     }
 
     // to limit calendar range
     canChangeNavMonth(numToChange: number): boolean {
         const clonedDate = moment(this.navDate);
         clonedDate.add(numToChange, 'month');
-        const minDate = moment().add(-1, 'month');
-        const maxDate = moment().add(1, 'month');
+        const minDate = moment().add(-4, 'month');
+        const maxDate = moment().add(12, 'month');
         // is between => moment method
         return clonedDate.isBetween(minDate, maxDate);
 
