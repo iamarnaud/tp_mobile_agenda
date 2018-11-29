a3326a4eb9b06bbd70dc82fc9129da5bf06bf62c
IpCeSs <ceciliapigeolet@gmail.com>
2018-11-29 18:18:55 +0100

    minor fix bug after update event

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -14,33 +14,31 @@ const httpOptions = {
     providedIn: 'root'
 })
 export class EventService {
-    
-    private connectionUrl = 'https://pagenda.alwaysdata.net/data/pagenda_calendar/_all_docs?include_docs=true';
-    // Couhdb API CALL to create document
-    private url = 'https://pagenda.alwaysdata.net/data/pagenda_calendar/';
+    // Couchdb API CALL url
+    private connectionUrl = 'https://pagenda.alwaysdata.net/data/pagenda_calendar/';
     constructor(private http: HttpClient) { }
 
     getAllEvents(): Observable<any> {
-        return this.http.get<any>(this.connectionUrl);
+        return this.http.get<any>(this.connectionUrl+'_all_docs?include_docs=true');
     }
     addEvent(evt: object): Observable<any> {
-        return this.http.post<Response>(this.url, evt, httpOptions);
+        return this.http.post<Response>(this.connectionUrl, evt, httpOptions);
     }
     deleteEvent(docId, revision): Observable<any> {
-        return this.http.delete('https://pagenda.alwaysdata.net/data/pagenda_calendar/'+docId+'?rev='+revision, httpOptions)
+        return this.http.delete(this.connectionUrl+docId+'?rev='+revision, httpOptions)
     }
     updateEvent(docId, revision, form): Observable<any> {
-        return this.http.put<Response>('https://pagenda.alwaysdata.net/data/pagenda_calendar/'+docId+'?rev='+revision, form, httpOptions)
+        return this.http.put<Response>(this.connectionUrl+docId+'?rev='+revision, form, httpOptions)
     }
     getEvent(docId): Observable<any> {
-        return this.http.get<any>('https://pagenda.alwaysdata.net/data/pagenda_calendar/'+docId);
+        return this.http.get<any>(this.connectionUrl+docId);
     }
     addComer(docId, user,revision, form) {
         form.participants.push(user);
-        return this.http.put<Response>('https://pagenda.alwaysdata.net/data/pagenda_calendar/'+docId+'?rev='+revision, form, httpOptions);
+        return this.http.put<Response>(this.connectionUrl+docId+'?rev='+revision, form, httpOptions);
     }
     removeComer(docId, user,revision, form, index) {
         form.participants.splice(index, 1);
-        return this.http.put<Response>('https://pagenda.alwaysdata.net/data/pagenda_calendar/'+docId+'?rev='+revision, form, httpOptions);
+        return this.http.put<Response>(this.connectionUrl+docId+'?rev='+revision, form, httpOptions);
     }
 }
